908f95dcb6e58a5e9b8f7072dc651a135e941f9f
Atlon1 <maciej.szajstek@gmail.com>
2023-06-01 23:02:49 +0200

    tsection about is rdy

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
+import {about} from '../data';
+import {IoIosArrowDroprightCircle} from "react-icons/io";
+
 const About = () => {
+    const {title, subtitle1, subtitle2, icon, link} = about
+
     return (
-        <div>
-            About
-        </div>
+        <section className=' py-[80px] md:py-[110px] lg:pt-[140px] lg:pb-[180px]'>
+            <div className='container mx-auto px-[20px] lg:px-[135px]'>
+                <div
+                    data-aos='fade-up'
+                    data-aos-dely='100'
+                    className='section-title-group justify-start'>
+                    <img src={icon} alt='icon'/>
+                    <h2 className='h2 section-title'>{title}
+                        <span className='text-primary-200'>
+                        .
+                    </span>
+                    </h2>
+                </div>
+                <p
+                    data-aos='fade-up'
+                    data-aos-dely='200'
+                    className='md:text-body-md mb-12'>{subtitle1}</p>
+                <p  data-aos='fade-up'
+                    data-aos-dely='300'
+                    className='md:text-body-md mb-8'>{subtitle2}</p>
+                <div
+                    data-aos='fade-up'
+                    data-aos-dely='400'>
+                    <a className='link flex items-center gap-x-4 hover:gap-x-6 transition-all'
+                       href='#'>{link}
+                        <IoIosArrowDroprightCircle className='text-2xl'/>
+                    </a>
+                </div>
+            </div>
+        </section>
     );
 };
 
