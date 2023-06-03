f8c30add2f05a29adc6be87250008314e4baef6f
Atlon1 <maciej.szajstek@gmail.com>
2023-06-03 22:44:06 +0200

    section workouts rdy

--- src/components/WorkoutSlider.tsx
+++ src/components/WorkoutSlider.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 
+import {workouts} from "../data";
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../workoutSlider.css'
+
+import {Navigation} from "swiper";
+
 const WorkoutSlider = () => {
+
+    const {programs} = workouts
     return (
-        <div>
-            WorkoutSlider
-        </div>
+        <Swiper
+            slidesPerView={2}
+            spaceBetween={32}
+            navigation={true}
+            breakpoints={{
+                768: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 4,
+                },
+            }}
+            modules={[Navigation]}
+            className='workoutSlider'>
+            {programs.map((elem, index) => {
+                const {image, name} = elem
+                return (
+                    <SwiperSlide
+                        className='max-w-[320px] max-h-[320px] relative'
+                        key={index}>
+                        <img className='w-full h-full object-cover' src={image} alt='image'/>
+                        <div className='absolute left-[20px] bottom-[20px] bg-white h-[26px] px-[14px]
+                        flex items-center rounded-[1px]'>
+                            <div className='font-primary font-semibold text-sm text-neutral-500'>{name}</div>
+                        </div>
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
     );
 };
 
