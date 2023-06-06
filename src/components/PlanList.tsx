24ced1f1b833180aaaaacb8af1b6e79fc46c2529
Atlon1 <maciej.szajstek@gmail.com>
2023-06-06 21:44:19 +0200

    section pricing rdy

--- src/components/PlanList.tsx
+++ src/components/PlanList.tsx
@@ -25,7 +25,11 @@ const PlanList: FC<PlanListProps> = ({plans}) => {
                     <div
                         onClick={() => setIndex(currentIndex)}
                         className='w-full md:max-w-[620px] lg:max-w-[405px] rounded-sm px-4 lg:min-h-[550px]'
-                        key={currentIndex}>
+                        key={currentIndex}
+                        data-aos='fade-up'
+                        data-aos-offset ='200'
+                        data-aos-delay={delay}
+                        >
                         <div
                             className={`${currentIndex === index ? "bg-neutral-500 text-white" : 'bg-neutral-400/10 text-neutral-500'} 
                             flex justify-center items-center py-[40px] px-[30px] lg:min-h-[550px] transition duration-100`}>
@@ -36,12 +40,13 @@ const PlanList: FC<PlanListProps> = ({plans}) => {
                                     h-[26px] font-primary text-sm font-semibold min-w-min mx-auto px-[14px] flex items-center justify-center mb-8`}>
                                         {name}
                                     </div>
-                                    <div>
-                                        <div>
-                                            <span>{price}</span>
-                                            <span>$</span>
+                                    <div
+                                        className='text-[40px] lg:text-[50px] font-primary font-extrabold text-center flex flex-col items-center justify-center'>
+                                        <div className='leading-nones'>
+                                            <span className='tracking-[0.1px]'>{price}</span>
+                                            <span className='text-[30px] font-extrabold'>$</span>
                                         </div>
-                                        <span>/month</span>
+                                        <span className='text-sm font-medium'>/month</span>
                                     </div>
                                 </div>
                                 <div>
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -9,7 +9,11 @@ const Pricing = () => {
 
     return (
         <section className='section'>
-           <div className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0'>
+           <div className='section-title-group max-w-[540px] mx-auto px-4 lg:px-0'
+            data-aos='fade-up'
+                data-aos-offset="200"
+                data-aos-delay='200'
+           >
                <img src={icon} alt='icon'/>
                <h2 className='h2'>{title}<span className='text-primary-200'>.</span></h2>
            </div>
