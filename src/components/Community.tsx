d32aea0880f373e65444a403645c733458b11380
Atlon1 <maciej.szajstek@gmail.com>
2023-06-07 22:26:16 +0200

    section comuunity rdy

--- src/components/Community.tsx
+++ src/components/Community.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import {community} from "../data"
+import CommunitySlider from "./CommunitySlider";
+
 const Community = () => {
+
+    const {icon, title, testimonials} = community
+
     return (
-        <div>
-            Community
-        </div>
+        <section className='section relative'>
+            <div className='container mx-auto'>
+                <div className='flex'>
+                    <div className='section-title-group max-w-[240px] mx-auto px-4 lg:px-0 lg:ml-0 '>
+                        <img src={icon} alt='icon'/>
+                        <h2 className='h2 section-title'>{title}
+                            <span className='text-primary-200'>
+                                .
+                            </span>
+                        </h2>
+                    </div>
+                    <div>
+                        <CommunitySlider/>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
