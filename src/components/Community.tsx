42d1364ec67d9ae781506a530197acc92dac0f24
Atlon1 <maciej.szajstek@gmail.com>
2023-06-19 21:49:13 +0200

    section comunity rdy

--- src/components/Community.tsx
+++ src/components/Community.tsx
@@ -13,7 +13,11 @@ const Community= () => {
         <section className='section relative'>
             <div className='container mx-auto'>
                 <div className='flex'>
-                    <div className='section-title-group max-w-[240px] mx-auto px-4 lg:px-0 lg:ml-0 '>
+                    <div className='section-title-group max-w-[240px] mx-auto px-4 lg:px-0 lg:ml-0 '
+                    data-aos = "fade-up"
+                    data-aos-offset = "200"
+                    data-aos-delay = "200"
+                    >
                         <img src={icon} alt='icon'/>
                         <h2 className='h2 section-title'>{title}
                             <span className='text-primary-200'>
@@ -21,7 +25,11 @@ const Community= () => {
                             </span>
                         </h2>
                     </div>
-                    <div className='absolute -right-[375px] w-[1140px] top-48 lg:top-0'>
+                    <div className='absolute -right-[375px] lg:-right-[280px] w-[1140px] top-48 lg:top-0'
+                         data-aos = "fade-left"
+                         data-aos-offset = "200"
+                         data-aos-delay = "200"
+                    >
                         <CommunitySlider testimonials={testimonials}/>
                     </div>
                 </div>
--- src/components/CommunitySlider.tsx
+++ src/components/CommunitySlider.tsx
@@ -32,13 +32,12 @@ const CommunitySlider: FC<TestimonialProps> = ({testimonials}) => {
                             <div>
                                 <img src={image} alt={name}/>
                             </div>
-                            <div>
-                                <div>{message}</div>
-                                <div>
-                                    <span>~</span>
-                                    <div>{name}</div>
+                            <div className='absolute bottom-[30px] text-white p-[20px] text-center'>
+                                <div className='mb-8 italic text-lg font-light'>{message}</div>
+                                <div className='flex items-center justify-center gap-x-[3px]'>
+                                    <span className='text-[30px] text-primary-200 font-bold'>~</span>
+                                    <div className='text-[20px] font-bold'>{name}</div>
                                 </div>
-
                             </div>
                         </div>
                     </SwiperSlide>
