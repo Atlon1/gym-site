3fcba9fe5441177c5fc659b40d8ae840ebee1500
Atlon1 <maciej.szajstek@gmail.com>
2023-06-24 21:06:51 +0200

    faq prepare question and answers

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,13 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+
+import {FaChevronCircleUp, FaChevronCircleDown} from "react-icons/fa"
 
 interface AccordionProps {
     accordion: any
 }
 
 const Accordion: FC<AccordionProps>=  ({accordion}) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const {question,  answer} = accordion
+
     return (
-        <div>
-            Acorddion
+        <div className='cursor-pointer'>
+            <div className='bg-white border'>
+                acc
+            </div>
         </div>
     );
 };
--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -8,14 +8,13 @@ const Faq = () => {
     const {icon, title, accordions} = faq
     return (
         <section className='section pt-[480px] lg:pt-[280px]' id="faq">
-            <div>
-                <div>
+            <div className='max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:h-[1160px] lg:pt-6'>
+                <div className='section-title-group justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0'>
                     <img className='lg:hidden' src={icon} alt="icon"/>
                     <h2 className='h2 section-title lg:mt-[100px]'>{title}
                         <span className='text-primary-200'>.</span>
                     </h2>
                 </div>
-
                 <div>
                     {accordions.map((accrdion, idx)=>{
                         return <Accordion accordion={accrdion} key={idx}/>
