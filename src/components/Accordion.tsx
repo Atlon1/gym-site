a0f0cdea0a31d738744e771ee3521ee3cd993546
Atlon1 <maciej.szajstek@gmail.com>
2023-06-24 22:14:47 +0200

    section FAQ is rdy

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -12,9 +12,24 @@ const Accordion: FC<AccordionProps>=  ({accordion}) => {
     const {question,  answer} = accordion
 
     return (
-        <div className='cursor-pointer'>
-            <div className='bg-white border'>
-                acc
+        <div onClick={() => setIsOpen(!isOpen)} className='cursor-pointer'>
+            <div className='bg-white border rounded-sm '>
+                <div className='min-h-[60px] flex items-center justify-between px-[30px]'>
+                    <h6 className='h6'>{question}</h6>
+                    <div>
+                        {isOpen ? (
+                            <FaChevronCircleUp className='text-[20px] text-neutral-500'/>
+                        ) : (
+                            <FaChevronCircleDown className='text-[20px] text-neutral-500'/>
+                        )}
+                    </div>
+                </div>
+                <div className={`${isOpen ? 'min-h-[200px] lg:min-h-[200px]': 'min-h-0'} max-h-0 overflow-hidden 
+                flex justify-center transition-all px-[30px]`}>
+                    <div className='mt-6'>
+                        {answer}
+                    </div>
+                </div>
             </div>
         </div>
     );
